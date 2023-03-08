719f7d7700eba5b9195a5d875d652b624dd3692e
Laura <laurakovacic@outlook.com>
2023-03-08 21:59:54 +0100

    add lesson1

---
+++ lesson1/owners/Owner.java
@@ -0,0 +1,14 @@
+package owners;
+
+import pets.*;
+
+public class Owner {
+
+	public static void main(String[] args) {
+
+		Cat mica = new Cat();
+		Dog cuko = new Dog();
+
+		System.out.println("Imam micu i cuku.");
+	}
+}
---
+++ lesson1/pets/Dog.java
@@ -0,0 +1,10 @@
+package pets;
+
+public class Dog {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Now accessing Dog class.");	
+
+	}
+}
